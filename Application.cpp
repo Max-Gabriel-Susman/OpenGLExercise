9a5e3c814330c7f558b344c7e275635ff18f992e
Prometheus <gabe.susman@gmail.com>
2021-03-28 15:07:16 -0600

    setup vertex attributes and a layout in open GL

--- Application.cpp
+++ Application.cpp
@@ -10,8 +10,6 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
-	
-
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -38,6 +36,11 @@ int main(void)
 	glGenBuffers(1, &buffer); // generation
 	glBindBuffer(GL_ARRAY_BUFFER, buffer); // binding
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // initialize
+
+	glEnableVertexAttribArray(0);	
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	/* Loop until the user closes the window */		
 	while (!glfwWindowShouldClose(window))
